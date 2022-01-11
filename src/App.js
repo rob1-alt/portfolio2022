d92ebbcfd76b85a207007308af357a09c35a1963
Rob1 <robin.pautigny@hetic.net>
2022-01-11 11:31:13 +0100

    change root directory 🛣

--- src/App.js
+++ src/App.js
@@ -1,22 +1,19 @@
-import logo from './logo.svg';
 import './App.css';
+import { Helmet } from 'react-helmet'
+
+
+const title = 'Robin Pautigny'
 
 function App() {
   return (
     <div className="App">
+      <Helmet>
+        <title>{title}</title>
+      </Helmet>
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h3>Bonjour, Je suis Robin Pautigny - étudiant en 3ème année à Hetic.
+          Je m’intéresse aux nouvelles technologies, aux voitures ainsi qu’au marketing. Actuellement à la recherche d’un stage en tant que Chef de projet digital.
+        </h3>
       </header>
     </div>
   );

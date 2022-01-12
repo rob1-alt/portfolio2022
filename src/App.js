db4d75dacf719426e33dc43fe9af3bb9bb619a06
Rob1 <robin.pautigny@hetic.net>
2022-01-12 22:38:53 +0100

    first menu works 🛠

--- src/App.js
+++ src/App.js
@@ -11,9 +11,24 @@ function App() {
         <title>{title}</title>
       </Helmet>
       <header className="App-header">
-        <h3>Bonjour, Je suis Robin Pautigny - étudiant en 3ème année à Hetic.
-          Je m’intéresse aux nouvelles technologies, aux voitures ainsi qu’au marketing. Actuellement à la recherche d’un stage en tant que Chef de projet digital.
-        </h3>
+        <div className='wrapperHomepage'>
+          <div className='halfRight'>
+            <h3>
+              Bonjour, Je suis Robin Pautigny - étudiant en 3ème année à Hetic.
+              Je m’intéresse aux nouvelles technologies, aux voitures ainsi qu’au marketing. Actuellement à la recherche d’un stage en tant que Chef de projet digital.
+            </h3>
+          </div>
+          <div className='halfLeft'>
+            <nav className='menuNav'>
+              <ul>
+                <li>À propos de moi</li>
+                <li>Projets</li>
+                <li>Contact</li>
+              </ul>
+            </nav>
+
+          </div>
+        </div>
       </header>
     </div>
   );
